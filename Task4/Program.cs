d5a30572a69a819c07b7448ec27dcff6108b1ab1
Beksultan <bekaabaevich@gmail.com>
2024-07-10 03:04:29 +0500

    Task4 was added

---
+++ Task4/Program.cs
@@ -0,0 +1,22 @@
+﻿int a = 25;
+int b = 100;
+int c = 7;
+int d = 15;
+int e = 0;
+
+
+int max = a;
+if(b>max){
+    max = b;
+}
+if(c>max){
+    max = c;
+}
+if(d>max){
+    max = d;
+}
+if(e>max){
+    max = e;
+}
+
+Console.WriteLine(max);
